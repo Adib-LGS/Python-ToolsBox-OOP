b94180adba565ce62d0cf86065e0fb622ca0ed9e
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-07-02 16:05:21 +0100

    add @classmethod

--- formation_python/OOP/human.py
+++ formation_python/OOP/human.py
@@ -1,10 +1,13 @@
-###Source class
+###Source class###
 import datetime
 
 
 class Human:
 
+    # class attributes increase with new created human
     human_created = 0
+
+    humans_planet = "Hearth"
 
     def __init__(self, name, sex, birth):
         self.name = name
@@ -15,18 +18,26 @@ class Human:
         if Human:
             self.human_created += 1
     
+
     def create_human(self):
         list_of_human = self.list_of_humans
         list_of_human.extend([self.name, self.sex, self.birth])
         print(list_of_human)
     
+
     def get_humans_age(self):
         today = int(datetime.datetime.now().strftime("%G"))
         age = today - int(self.birth)
         print(f"{self.name}'s age is {age} years old")
+
+    @classmethod
+    def change_humans_planet(cls, new_planet_name = "Heart"):
+        Human.humans_planet = new_planet_name
+        return new_planet_name
 
 
 h1 = Human(name="first", sex="Male", birth=1)
 h1.create_human()
 h1.get_humans_age()
 print(f"Numbers of human being created: {h1.human_created}")
+print(f"{h1.name} was laid on {h1.change_humans_planet('Mars')}")
